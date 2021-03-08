f2ea5346792b9329c578fb994025769078066009
ASUS <ASUS@LAPTOP-SIV1743K>
2021-03-08 23:56:07 +0530

    test

--- src/main/java/RestAssuredProject/RestAssuredProject/Reverse.java
+++ src/main/java/RestAssuredProject/RestAssuredProject/Reverse.java
@@ -6,7 +6,7 @@ public class Reverse {
 		String input="dev";
 		char[] try1=input.toCharArray();
 		
-		for(int i= try1.length -1;i>=0; i--)
+		for(int i= try1.length -1;i>=0; i++)
 		
 		
 System.out.println( try1);
